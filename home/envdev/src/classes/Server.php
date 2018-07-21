02010dc860dc526ce6a6138318b13d18686d8020
Vincent Faliès <vincent@vfac.fr>
2018-07-21 17:26:29 +0200

    Add version on Homepage

--- home/envdev/src/classes/Server.php
+++ home/envdev/src/classes/Server.php
@@ -15,9 +15,9 @@ class Server
     public function getInformations()
     {
         $this->informations->php       = getenv('PHP_VERSION');
-        $this->informations->webserver = getenv('WEB_SERVER');
-        $this->informations->cache     = getenv('CACHE_SERVER');
-        $this->informations->db        = getenv('DB');
+        $this->informations->webserver = getenv('WEB_SERVER').' ('.getenv('WEB_SERVER_VERSION').')';
+        $this->informations->cache     = getenv('CACHE_SERVER').' ('.getenv('CACHE_VERSION').')';
+        $this->informations->db        = getenv('DB').' ('.getenv('DB_VERSION').')';
 
         return $this->informations;
     }
