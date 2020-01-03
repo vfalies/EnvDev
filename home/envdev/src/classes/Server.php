a2948b2938df9bd2c8837f5ee0c44df526787f42
Vincent Faliès <vincent@vfac.fr>
2020-01-03 11:30:23 +0100

    Update servers versions

--- home/envdev/src/classes/Server.php
+++ home/envdev/src/classes/Server.php
@@ -17,8 +17,10 @@ class Server
         $this->informations->php               = getenv('PHP_VERSION');
         $this->informations->webserver         = getenv('WEB_SERVER');
         $this->informations->webserver_version = getenv('WEB_SERVER_VERSION');
-        $this->informations->cache             = getenv('CACHE_SERVER') . ' (' . getenv('CACHE_VERSION') . ')';
-        $this->informations->db                = getenv('DB') . ' (' . getenv('DB_VERSION') . ')';
+        $this->informations->cache             = getenv('CACHE_SERVER');
+        $this->informations->cache_version     = getenv('CACHE_VERSION');
+        $this->informations->db                = getenv('DB');
+        $this->informations->db_version        = getenv('DB_VERSION');
         $this->informations->queuer            = getenv('QUEUER_SERVER');
 
         return $this->informations;
