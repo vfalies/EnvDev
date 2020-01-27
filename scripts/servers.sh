3635072cfe9d17677bce37cd33e8a1c4afc80140
Vincent Faliès <vincent@vfac.fr>
2020-01-27 15:33:52 +0100

    Add homepage launching at the end of default servers running

--- scripts/servers.sh
+++ scripts/servers.sh
@@ -2,6 +2,7 @@
 
 if test -f "./profiles/default.env"; then
     ./scripts/containers.sh ./profiles/default.env
+    ./scripts/homepage.sh
 else
     echo "No default profile found. Type the command 'make profile' to create it."
 fi
